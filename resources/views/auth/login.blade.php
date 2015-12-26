ef8938d20cd96fc3ab14d99c2798bcba1bd194b2
TeerasakGame <manu98765@hotmail.com>
2015-12-27 00:41:18 +0700

    แก้ข้อความภาษาไทยหน้า login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,8 @@
 	<div class="row">
 		<div class="col-md-8 col-md-offset-2">
 			<div class="panel panel-default">
-				<div class="panel-heading">Login</div>
+				<!--<div class="panel-heading">Login</div>-->
+				<div class="panel-heading">เข้าสู่ระบบ</div>
 				<div class="panel-body">
 					@if (count($errors) > 0)
 						<div class="alert alert-danger">
@@ -23,14 +24,16 @@
 						<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
 						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
+							<!--<label class="col-md-4 control-label">E-Mail Address</label>-->
+							<label class="col-md-4 control-label">อีเมล</label>
 							<div class="col-md-6">
 								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
 							</div>
 						</div>
 
 						<div class="form-group">
-							<label class="col-md-4 control-label">Password</label>
+							<!--<label class="col-md-4 control-label">Password</label>-->
+							<label class="col-md-4 control-label">รหัสผ่าน</label>
 							<div class="col-md-6">
 								<input type="password" class="form-control" name="password">
 							</div>
@@ -48,7 +51,8 @@
 
 						<div class="form-group">
 							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">Login</button>
+								<!--<button type="submit" class="btn btn-primary">Login</button>-->
+								<button type="submit" class="btn btn-primary">เข้าสู่ระบบ</button>
 
 								<!--<a class="btn btn-link" href="{{ url('/password/email') }}">ลืมรหัสผ่าน</a>-->
 							</div>
